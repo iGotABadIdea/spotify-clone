bda41a59cd25b668656ec79f936ea68c406a1824
iGotABadIdea <rv17314@gmail.com>
2022-01-25 02:33:02 +0530

    implementing auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,12 @@ import Sidebar from "../components/Sidebar"
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-black h-screen overflow-hidden">
       <Head>
         <title>Spotify-clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1> This is a spotify clone </h1>
-      <main>
+      <main className="">
         <Sidebar />
       </main>
       <div>{/*player*/}</div>
---
+++ pages/login.jsx
@@ -0,0 +1,8 @@
+function Login() {
+    return(
+    <div>
+        <h1>THis is a login page </h1>
+    </div>
+    )
+}
+export default Login
